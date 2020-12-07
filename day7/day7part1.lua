40ecfe25a81d9ebdb7bd139f01361ccc3e8b74d0
LordFarquadTheGreat <moniquetamere1@gmail.com>
2020-12-07 16:49:07 -0500

    Updates day 7 input handling

--- day7/day7part1.lua
+++ day7/day7part1.lua
@@ -2,22 +2,14 @@
 io.input(io.open("input"))
 
 --set input in an array
-cdforms = {}
+rules = {}
 index = 1
 for line in io.lines() do
-    if not cdforms[index] then cdforms[index] = {} cdforms[index].sum = 0 end
-    for q in line:gmatch"." do 
-        if not cdforms[index][q] then cdforms[index][q] = true
-        cdforms[index].sum = cdforms[index].sum + 1 end end
-    if line == "" then index = index + 1 end
+    rules[index] = {}
+    rules[index].container, contents = line:match("(.+)%sbags%scontain%s(.+)%.")
+    index = index + 1
 end
 
 io.close()
 
 
---count number of questions
-sum = 0
-for index, cdform in ipairs(cdforms) do
-    sum = sum + cdform.sum
-end
-print("Sum of group yes questions is "..sum)
