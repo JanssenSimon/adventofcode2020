bb135eee7cf8be6909e2f2b76a7b614a0223ece1
LordFarquadTheGreat <moniquetamere1@gmail.com>
2020-12-08 18:21:09 -0500

    Completes part 2 of day 7

---
+++ day7/day7part2.lua
@@ -0,0 +1,25 @@
+--get input file
+io.input(io.open("input"))
+
+--set input in an array
+bagsin = {}
+for line in io.lines() do
+    container, contents = line:match("(.+)%sbags%scontain%s(.+)%.")
+    bagsin[container] = {}
+    for number, bagcolour in contents:gmatch("(%d)%s(%a+%s%a+)%sbag") do
+        table.insert(bagsin[container], {c=bagcolour, n=number})
+    end
+end
+
+io.close()
+
+
+--count total amount of bags inside of shiny gold one
+function countbagsin(bagtype)
+    sum = 0
+    for i, nestedbag in ipairs(bagsin[bagtype]) do
+        sum = sum + tonumber(nestedbag.n) + tonumber(nestedbag.n) * countbagsin(nestedbag.c)
+    end
+    return sum
+end
+print("The total amount of bags in the gold one is: "..countbagsin("shiny gold"))
