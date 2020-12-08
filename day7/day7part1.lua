cf950fc53accfc4e89b2ba51466c5cbae811e5bc
LordFarquadTheGreat <moniquetamere1@gmail.com>
2020-12-08 17:34:39 -0500

    Completes part one of day 7

--- day7/day7part1.lua
+++ day7/day7part1.lua
@@ -2,14 +2,30 @@
 io.input(io.open("input"))
 
 --set input in an array
-rules = {}
-index = 1
+contains = {}
 for line in io.lines() do
-    rules[index] = {}
-    rules[index].container, contents = line:match("(.+)%sbags%scontain%s(.+)%.")
-    index = index + 1
+    container, contents = line:match("(.+)%sbags%scontain%s(.+)%.")
+    for bagtype in contents:gmatch("%d%s(%a+%s%a+)%sbag") do
+        if not contains[bagtype] then contains[bagtype] = {} end
+        table.insert(contains[bagtype], container)
+    end
 end
 
 io.close()
 
 
+count = 0
+counted = {}
+function countcontainers(bagtype)
+    if contains[bagtype] then
+        for i, container in ipairs(contains[bagtype]) do
+            if not counted[container] then
+                counted[container] = true
+                count = count + 1
+                countcontainers(container)
+            end
+        end
+    end
+end
+countcontainers("shiny gold")
+print("The number of types of bags which can ultimately contain a gold one is: "..count)
