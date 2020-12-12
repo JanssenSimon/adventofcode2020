690b83afbdfc0ac1a87be0e4817b1c75c17d9972
LordFarquadTheGreat <moniquetamere1@gmail.com>
2020-12-12 15:19:40 -0500

    Adds part 1 of day 11

--- day11/day11part1.lua
+++ day11/day11part1.lua
@@ -5,17 +5,84 @@ function open(filename, lineoperation)
 end
 function toarray(line)
     output = {}
-    for character in str:gmatch"." do table.insert(output, character) end
+    for character in line:gmatch"." do table.insert(output, character) end
+    return output
 end
 
 input = {}
 open("input", function(line) table.insert(input, toarray(line)) end)
-table.sort(input)
 
 
+function getsumofsurrounding(li, i, state)
+    state = state or input
+    sumofsurrounding = 0
+    if state[li-1] then
+        if state[li-1][i-1] and state[li-1][i-1] == "#" then 
+                sumofsurrounding = sumofsurrounding + 1 end
+        if state[li-1][i] == "#" then
+            sumofsurrounding = sumofsurrounding + 1 end
+        if state[li-1][i+1] and state[li-1][i+1] == "#" then 
+                sumofsurrounding = sumofsurrounding + 1 end
+    end
+    if state[li][i-1] and state[li][i-1] == "#" then 
+            sumofsurrounding = sumofsurrounding + 1 end
+    if state[li][i+1] and state[li][i+1] == "#" then 
+            sumofsurrounding = sumofsurrounding + 1 end
+    if state[li+1] then
+        if state[li+1][i-1] and state[li+1][i-1] == "#" then 
+                sumofsurrounding = sumofsurrounding + 1 end
+        if state[li+1][i] == "#" then
+            sumofsurrounding = sumofsurrounding + 1 end
+        if state[li+1][i+1] and state[li+1][i+1] == "#" then 
+                sumofsurrounding = sumofsurrounding + 1 end
+    end
+    return sumofsurrounding
+end
 function getstateafter(state)
     nextstate = {}
-    for lineindex, line in state do
-        
+    for lineindex, line in ipairs(state) do
+        nextstate[lineindex] = {}
+        for index, character in ipairs(line) do
+            newcharacter = '.'
+            if character == 'L' then
+                shouldbefull = getsumofsurrounding(lineindex,index,state) == 0
+                if shouldbefull then newcharacter = '#'
+                else newcharacter = 'L' end
+            elseif character == '#' then
+                shouldbeempty = getsumofsurrounding(lineindex,index,state) >= 4
+                if shouldbeempty then newcharacter = 'L'
+                else newcharacter = '#' end
+            end
+            nextstate[lineindex][index] = newcharacter
+        end
     end
+    return nextstate
 end
+function printstate(state)
+    for lineindex, line in ipairs(state) do print(table.concat(line, "")) end
+end
+function areequaltables(table1, table2)
+    for k, v in pairs(table1) do 
+        if type(v) == 'table' then 
+            if not areequaltables(v, table2[k]) then return false end
+        elseif table2[k] ~= v then return false end
+    end
+    return true
+end
+function countoccupiedseats(state)
+    count = 0
+    for lineindex, line in ipairs(state) do 
+        for index, char in ipairs(line) do 
+            if char == "#" then count = count + 1 end
+        end
+    end
+    return count
+end
+
+state = input
+newstate = getstateafter(input)
+while not areequaltables(newstate, state) do
+    state = newstate
+    newstate = getstateafter(newstate)
+end
+print("When the state stabilizes, the number of occupied seats is "..countoccupiedseats(state))
