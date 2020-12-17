be5d479ef0c8fed76da651db847bc9f7d77bb506
LordFarquadTheGreat <moniquetamere1@gmail.com>
2020-12-16 22:47:53 -0500

    Adds part 1 of day 15

---
+++ day15/day15part1.lua
@@ -0,0 +1,29 @@
+history = {}
+
+--puzzle input, starting numbers
+table.insert(history, 10)
+table.insert(history, 16)
+table.insert(history, 6)
+table.insert(history, 0)
+table.insert(history, 1)
+table.insert(history, 17)
+
+
+while #history ~= 2020 do
+    lastelem = history[#history]
+    notunique = false
+    lastindex = 1
+    for i, elem in ipairs(history) do
+        if elem == lastelem and i ~= #history then
+            --print("I've seen "..elem.." before at "..i)
+            notunique = true
+            lastindex = i
+        end
+    end
+    if notunique then table.insert(history, #history-lastindex)
+    else table.insert(history, 0) end
+end
+
+for i, elem in ipairs(history) do
+    print(i.." "..elem)
+end
